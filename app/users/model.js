c41b89afd86eba16e2a91cdf9258ecd16d433acc
Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-03-21 23:36:36 +0700

    First Commit

---
+++ app/users/model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+let userSchema = mongoose.Schema({
+	username: String,
+	password: String,
+	nama: String,
+	npm: Number,
+	ttl: String,
+	tgl: String,
+	agama: String,
+	hp: String,
+	goldar: String,
+	email: String,
+	rumah: String,
+	kos: String,
+	pendidikan: String,
+	panitia: String,
+	organisasi: String,
+	pelatihan: String,
+	prestasi: String,
+	role: String,
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);

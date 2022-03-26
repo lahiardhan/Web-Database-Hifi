d48156395a0a45140ef5000df7b2ebd9733a34e7
Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-03-26 12:03:25 +0700

    add users/controller.js

--- app/users/controller.js
+++ app/users/controller.js
@@ -1,11 +1,76 @@
+const res = require("express/lib/response");
+
 module.exports={
    index: async (req, res) => {
       try {
-         res.render('form', {
+         res.render('home', {
             title: "datahifi"
          });
       } catch (err) {
          console.log(err);
       }
-   }
+   },
+
+   viewLogIn: async (req, res) => {
+      try {
+         res.render('login');
+      } catch (err) {
+         console.log(err);
+      }
+   },
+   
+   viewSignUp: async (req, res) => {
+      try {
+         res.render('signup');
+      } catch (err) {
+         console.log(err);
+      }
+   },
+
+   viewForm: async (req, res) => {
+      try {
+         res.render('form');
+      } catch (err) {
+         console.log(err);
+      }
+   },
+   
+   viewProfile: async (req, res) => {
+      try {
+         res.render('profile');
+      } catch (err) {
+         console.log(err);
+      }
+   },
+
+   viewForgot: async (req, res) => {
+      try {
+         res.render('forgot');
+      } catch (err) {
+         console.log(err);
+      }
+   },
+
+   viewDbs: async (req, res) => {
+      try {
+         res.render('dbs');
+      } catch (err) {
+         console.log(err);
+      }
+   },
+
+   actionLogOut: async (req, res) => {},
+
+   actionLogIn: async(req, res) => {},
+
+   actionSignUp: async(req, res) => {},
+
+   actionForm: async(req, res) => {},
+
+   actionProfile: async(req, res) => {},
+   
+   actionForgot: async(req, res) => {},
+   
+   actionDbs: async(req, res) => {},
+
 }

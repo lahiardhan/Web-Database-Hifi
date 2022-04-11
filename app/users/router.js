fd47efc1f167ea0fd221b3cd0ff5697c9b929412
Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-04-11 16:24:52 +0700

    menambahkan routing semua controller view

--- app/users/router.js
+++ app/users/router.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const { index, viewForm, viewProfile, viewDbs } = require('./controller');
+const { index, viewForm, viewProfile, viewDbs, viewLogIn, viewSignUp, viewForgot } = require('./controller');
 
 /* GET home page. */
 router.get('/', index);
+router.get('/login', viewLogIn);
+router.get('/signup', viewSignUp);
 router.get('/form', viewForm);
 router.get('/profile', viewProfile);
+router.get('/forgot', viewForgot);
 router.get('/dashboard', viewDbs);
 
 module.exports = router;

d90bda6fe48b6b703cbd1420e7bb1dfacc492152
Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-04-18 17:14:55 +0700

    menambahkan router baru : authRouter

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require('method-override');
 // router
 const usersRouter = require('./app/users/router');
 const dashboardRouter = require('./app/dashboard/router');
+const authRouter = require('./app/auth/router');
 
 const app = express();
 
@@ -34,6 +35,7 @@ app.use('/adminlte', express.static(path.join(__dirname, '/node_modules/admin-lt
 
 app.use('/', usersRouter);
 app.use('/dashboard', dashboardRouter);
+app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

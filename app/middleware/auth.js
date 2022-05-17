3b208fdd5617667d455af4edc3a2ae6a1a26ff18
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-05-17 14:02:58 +0700

    update message authentication middleware

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -3,7 +3,7 @@ module.exports = {
 		if (req.isAuthenticated()) {
 			return next();
 		}
-		req.flash("alertMessage", "Silahkan lakukan login terlebih dulu.");
+		req.flash("alertMessage", "Silahkan login terlebih dulu.");
       req.flash('alertStatus', 'danger');
 		res.redirect("/auth/login");
 	},

3072c20e87ee4beb336b962ef8aa98c2ee3221ad
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-05-17 13:10:09 +0700

    membuat fitur forgot password

--- app/users/controller.js
+++ app/users/controller.js
@@ -27,18 +27,62 @@ module.exports={
       }
    },
 
-   viewForgot: async (req, res) => {
-      try {
-         res.render('forgot');
-      } catch (err) {
-         console.log(err);
-      }
-   },
-
    actionForm: async(req, res) => {},
 
    actionProfile: async(req, res) => {},
    
-   actionForgot: async(req, res) => {},
+   
+   viewForgot: async (req, res) => {
+		try {
+			res.render("forgot", {
+            title: 'Ubah Password | Database HIFI',
+            message: req.flash('alertMessage'),
+            status: req.flash('alertStatus')
+         });
+		} catch (err) {
+			console.log(err);
+		}
+	},
+
+	actionForgot: async (req, res, next) => {
+      try {
+         const { username, email, password } = req.body;
+
+         const user = await User.findOne({ username: username });
+
+         if(user){
+            if(user.email == email) {
+               const updated = ({password})
+               bcrypt.genSalt(10, (err, salt) => {
+                  bcrypt.hash(updated.password, salt, (err, hash) => {
+                     if(err) throw(err);
+                     // save pass to hash
+                     updated.password = hash;
+                     User.findOneAndUpdate({username: username}, {password: updated.password}).then(() => {
+                        console.log(user);
+                        req.flash('alertMessage','Berhasil merubah password! Silahkan login kembali.');
+                        req.flash('alertStatus', 'success');
+                        res.redirect('/auth/login');
+                     })
+                  })
+               })
+
+            } else {
+               req.flash('alertMessage','Email yang dimasukkan salah!');
+               req.flash('alertStatus', 'danger');
+               res.redirect('/forgot');
+            }
+         }
+         else{
+            req.flash('alertMessage','Username tidak ditemukan!');
+            req.flash('alertStatus', 'danger');
+            res.redirect('/forgot');
+         }
+      } catch (err) {
+         req.flash('alertMessage', `${err.message}`);
+         req.flash('alertStatus', 'danger');
+         res.redirect('/auth/login');
+      }
+	},
    
 }
--- app/users/router.js
+++ app/users/router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { index, viewForm, viewProfile, viewForgot } = require('./controller');
+const { index, viewForm, viewProfile, viewForgot, actionForgot } = require('./controller');
 const { ensureAuthenticated } = require('../middleware/auth');
 
 router.get('/', index);
 router.get('/form', ensureAuthenticated,viewForm);
 router.get('/profile', ensureAuthenticated, viewProfile);
 router.get('/forgot', viewForgot);
+router.post('/forgot', actionForgot);
 
 module.exports = router;

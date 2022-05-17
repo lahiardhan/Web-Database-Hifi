9c37ccd2aa0f0c8ffeef68d1265469c57d0cb847
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-05-17 13:43:16 +0700

    update message middleware authentication halaman dashboard

--- app/dashboard/auth.js
+++ app/dashboard/auth.js
@@ -3,10 +3,14 @@ module.exports = {
 		if (req.isAuthenticated()) {
          if (req.user.role === "admin") {
             return next();
-         }
+			} else {
+				req.flash("alertMessage", "You're not authorized to access this recourse.");
+				req.flash('alertStatus', 'danger');
+				res.redirect("/auth/login");
+			}
 		}
-		req.flash("alertMessage", "You're not authorized to access this recourse.");
-      req.flash('alertStatus', 'danger');
+		req.flash("alertMessage", "Silahkan login terlebih dulu.");
+		req.flash('alertStatus', 'danger');
 		res.redirect("/auth/login");
 	},
 };

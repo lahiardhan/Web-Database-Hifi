aced9354636412eaad1dbe4d22d492213cfce337
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-05-20 22:35:08 +0700

    memberikan javascript untuk action close alert

--- public/javascript/dom.js
+++ public/javascript/dom.js
@@ -12,3 +12,24 @@ seePass.onclick = () => {
       seePassIcon.classList.add('fa-eye');
    }
 }
+
+// target element that will be dismissed
+const targetEl = document.getElementById('targetElement');
+
+// options object
+const options = {
+  triggerEl: document.getElementById('triggerElement'),
+  transition: 'transition-opacity',
+  duration: 1000,
+  timing: 'ease-out',
+
+  // callback functions
+  onHide: (context, targetEl) => {
+    console.log('element has been dismissed')
+    console.log(targetEl)
+  }
+};
+
+const dismiss = new Dismiss(targetEl, options);
+// hide the target element
+dismiss.hide();

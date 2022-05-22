ff185a4555c23560ef376936c9f8a9cfb7fe3f76
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-05-22 21:21:04 +0700

    delete unuse comment

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req,res,next)=> {
 next();
 })
 
-app.use(logger('dev'));             // bisa diapus gak nih
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
